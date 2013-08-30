773c133d937f849eaef5d92d15a2add55f5a0990
Raymond Zhou <somecrazyidiot@gmail.com>
2013-08-30 01:27:40 -0400

    Add working circle from three points algorithm.

--- sphere-shadow/js/main.js
+++ sphere-shadow/js/main.js
@@ -6,6 +6,9 @@ $(function() {
   var offsets = [];
   var stage = [];
 
+  var PI2 = 2 * Math.PI;
+  var DEG_TO_RAD = Math.PI / 180;
+
   var mouse = {
     x: null,
     y: null
@@ -44,6 +47,22 @@ $(function() {
     stage.forEach(function( object ) {
       object.draw( ctx );
     });
+
+    var circle = circleFromPoints(
+      stage[0].x, stage[0].y,
+      stage[1].x, stage[1].y,
+      stage[2].x, stage[2].y
+    );
+
+    if ( circle ) {
+      ctx.beginPath();
+      ctx.arc( circle.x, circle.y, circle.radius, 0, PI2 );
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'red';
+      ctx.stroke();
+    }
+
+    // console.log(circle.x, circle.y, circle.radius)
   }
 
   function mousePosition( event ) {
@@ -90,21 +109,85 @@ $(function() {
     /**
      * Given the equation of a circle located at (xc, yc) and with a radius r:
      *
-     * (x - xc)^2 + (y - yc)^2 - r^2 = 0
+     * (x - xc)^2 + (y - yc)^2 = r^2
      *
      * We substitute the coords of the three points, and get three equations
      * for three unknowns:
      *
-     * (x0 - xc)^2 + (y0 - yc)^2 - r^2 = 0
-     * (x1 - xc)^2 + (y1 - yc)^2 - r^2 = 0
-     * (x2 - xc)^2 + (y2 - yc)^2 - r^2 = 0
+     * (x0 - xc)^2 + (y0 - yc)^2 = r^2
+     * (x1 - xc)^2 + (y1 - yc)^2 = r^2
+     * (x2 - xc)^2 + (y2 - yc)^2 = r^2
      *
-     * Expanding this results in:
+     * By subtracting the first from the second and third equations and
+     * constructing a system of linear equations. With  some algebraic
+     * manipulation, we eventually get:
      *
-     * x0^2 - 2(x0 * xc) + xc^2 + xc^2 + ... = 0
+     * xc = [(x1^2 - y0^2) + (y1^2 - y0^2)] - (y1 - y0) /(y2 - y0) * [(x2^2 - x0^2) + (y2^2 - y0^2)]
+     *      ----------------------------------------------------------------------------------------
+     *                           2[x1 - x0 - (y1 - y0) / (y2 - y0) * (x2 - x0)]
      *
-     * Subtract the expanded versions
+     * yc = [(x1^2 - y0^2) + (y1^2 - y0^2)] - (x1 - x0) /(x2 - x0) * [(x2^2 - x0^2) + (y2^2 - y0^2)]
+     *      ----------------------------------------------------------------------------------------
+     *                           2[y1 - y0 - (x1 - x0) / (x2 - x0) * (y2 - y0)]
+     *
+     * Plug xc and yc back in to solve for the radius.
      */
+
+    var dx10 = x1 - x0,
+        dy10 = y1 - y0,
+        dx20 = x2 - x0,
+        dy20 = y2 - y0;
+
+    if ( dx20 === 0 || dy20 === 0 ) {
+      if ( dx10 === 0 || dy10 === 0 ) {
+        // Don't do anything if degenerate.
+        return;
+      }
+
+      // Swap vars so it can work.
+      var temp = x1;
+      x1 = x2;
+      x2 = temp;
+
+      temp = y1;
+      y1 = y2;
+      y2 = temp;
+
+      dx10 = x1 - x0;
+      dy10 = y1 - y0;
+      dx20 = x2 - x0;
+      dy20 = y2 - y0;
+    }
+
+    // Differences of squares. e.g. x1^2 - x0^2.
+    var dsqx10 = dx10 * ( x1 + x0 ),
+        dsqy10 = dy10 * ( y1 + y0 ),
+        dsqx20 = dx20 * ( x2 + x0 ),
+        dsqy20 = dy20 * ( y2 + y0 );
+
+    // Ratios, e.g. rdx10dx20 = (x1 - x0) / (x2 - x0).
+    var rdx10dx20 = dx10 / dx20,
+        rdy10dy20 = dy10 / dy20;
+
+    // Denominators.
+    var xden = 2 * ( dx10 - rdy10dy20 * dx20 ),
+        yden = 2 * ( dy10 - rdx10dx20 * dy20 );
+
+    if ( xden === 0 || yden === 0 ) {
+      return;
+    }
+
+    var x = ( ( dsqx10 + dsqy10 ) - rdy10dy20 * ( dsqx20 + dsqy20 ) ) / xden,
+        y = ( ( dsqx10 + dsqy10 ) - rdx10dx20 * ( dsqx20 + dsqy20 ) ) / yden;
+
+    // Now solve for the radius.
+    var r = Math.sqrt( ( x0 - x ) * ( x0 - x ) + ( y0 - y ) * ( y0 - y ) );
+
+    return {
+      x: x,
+      y: y,
+      radius: r
+    };
   }
 
   var $canvas = $( '#canvas' );
@@ -134,8 +217,6 @@ $(function() {
     ctx.fillStyle = '#222';
     ctx.fillRect( 0, 0, canvas.width, canvas.height );
 
-    var PI2 = 2 * Math.PI;
-    var DEG_TO_RAD = Math.PI / 180;
     var x = 0.5 * canvas.width;
     var y = 0.5 * canvas.height;
     var radius = 100;
