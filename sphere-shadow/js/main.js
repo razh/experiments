b9fca78fc02c58677fb4ebea94919e36a34b8602
Raymond Zhou <somecrazyidiot@gmail.com>
2013-08-30 20:04:43 -0400

    Work on UI, add Sphere class which will encapsulate most of the current functionality.

--- sphere-shadow/js/main.js
+++ sphere-shadow/js/main.js
@@ -14,7 +14,15 @@ $(function() {
     y: null
   };
 
-  var backgroundColor = '#222';
+  var config = {
+    handler: {
+      radius: 10
+    }
+  };
+
+  var backgroundColor = '#aaa';
+
+  var snapping = true;
 
   function Rect( x, y, width, height ) {
     this.x = x || 0.0;
@@ -35,6 +43,112 @@ $(function() {
            this.y <= y && y <= this.y + this.height;
   };
 
+  function Handler( x, y, radius ) {
+    this.x = x || 0.0;
+    this.y = y || 0.0;
+    this.radius = radius || 0.0;
+  }
+
+  Handler.prototype.draw = function( ctx ) {
+    // Draw outer circle.
+    ctx.beginPath();
+    ctx.arc( this.x, this.y, this.radius, 0, PI2 );
+    ctx.fillStyle = 'white';
+    ctx.fill();
+
+    // Draw inner circle.
+    ctx.beginPath();
+    ctx.arc( this.x, this.y, this.radius - 3, 0, PI2 );
+    ctx.fillStyle = '#222'; //'rgba(255, 0, 0, 1.0)';
+    ctx.fill();
+  };
+
+  Handler.prototype.contains = function( x, y ) {
+    var dx = x - this.x,
+        dy = y - this.y;
+
+    return Math.sqrt( dx * dx + dy * dy ) < this.radius;
+  };
+
+  function Sphere( x, y, radius ) {
+    this.x = x || 0.0;
+    this.y = y || 0.0;
+    this.radius = radius || 0.0;
+
+    this.handlers = [
+      // Start angle.
+      new Handler(
+        this.x + Math.cos( 315 * DEG_TO_RAD ) * this.radius,
+        this.y + Math.sin( 315 * DEG_TO_RAD ) * this.radius,
+        config.handler.radius
+      ),
+      // End angle.
+      new Handler(
+        this.x + Math.cos( 90 * DEG_TO_RAD ) * this.radius,
+        this.y + Math.sin( 90 * DEG_TO_RAD ) * this.radius,
+        config.handler.radius
+      ),
+      new Handler(
+        this.x + 0.5 * this.radius,
+        this.y + 0.5 * this.radius,
+        config.handler.radius
+      )
+    ];
+  }
+
+  Sphere.prototype.draw = function( ctx ) {
+    ctx.beginPath();
+    ctx.arc( this.x, this.y, this.radius, 0, PI2 );
+    ctx.fillStyle = 'rgba( 250, 120, 80, 1.0)';
+    ctx.fill();
+
+    var h0 = this.handlers[0],
+        h1 = this.handlers[1],
+        h2 = this.handlers[2];
+
+    if ( snapping ) {
+      var cp = closestPointOnCircle( h0.x, h0.y, this.x, this.y, this.radius );
+      h0.x = cp.x;
+      h0.y = cp.y;
+
+      cp = closestPointOnCircle( h1.x, h1.y, this.x, this.y, this.radius );
+      h1.x = cp.x;
+      h1.y = cp.y;
+
+      var perp = perpendicular( h0.x, h0.y, h1.x, h1.y );
+
+      ctx.beginPath();
+      ctx.moveTo( perp.x0, perp.y0 );
+      ctx.lineTo( perp.x1, perp.y1 );
+
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'white';
+      ctx.stroke();
+
+      cp = closestPointOnLine( h2.x, h2.y, perp.x0, perp.y0, perp.x1, perp.y1 );
+      h2.x = cp.x;
+      h2.y = cp.y;
+    }
+
+    this.handlers.forEach(function( handler ) {
+      handler.draw( ctx );
+    });
+
+    var circle = circleFromPoints(
+      h0.x, h0.y,
+      h1.x, h1.y,
+      h2.x, h2.y
+    );
+
+    if ( circle ) {
+      ctx.beginPath();
+      ctx.arc( circle.x, circle.y, circle.radius, 0, PI2 );
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'rgba(255, 0, 0, 0.5)';
+      ctx.stroke();
+    }
+  };
+
   function tick() {
     draw( context );
     requestAnimationFrame( tick );
@@ -48,6 +162,28 @@ $(function() {
     ctx.arc( bgCircle.x, bgCircle.y, bgCircle.radius, 0, PI2 );
     ctx.fillStyle = 'rgba(250, 120, 80, 1.0)';
     ctx.fill();
+
+    if ( snapping ) {
+      var closestPoint = closestPointOnCircle( stage[0].x, stage[0].y, bgCircle.x, bgCircle.y, bgCircle.radius );
+      stage[0].x = closestPoint.x;
+      stage[0].y = closestPoint.y;
+
+      closestPoint = closestPointOnCircle( stage[1].x, stage[1].y, bgCircle.x, bgCircle.y, bgCircle.radius );
+      stage[1].x = closestPoint.x;
+      stage[1].y = closestPoint.y;
+
+      var perp = perpendicular( stage[0].x, stage[0].y, stage[1].x, stage[1].y );
+      ctx.beginPath();
+      ctx.moveTo( perp.x0, perp.y0 );
+      ctx.lineTo( perp.x1, perp.y1 );
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'white';
+      ctx.stroke();
+
+      closestPoint = closestPointOnLine( stage[2].x, stage[2].y, perp.x0, perp.y0, perp.x1, perp.y1 );
+      stage[2].x = closestPoint.x;
+      stage[2].y = closestPoint.y;
+    }
 
     stage.forEach(function( object ) {
       object.draw( ctx );
@@ -73,13 +209,25 @@ $(function() {
 
       startAngle = angleFrom( circle.x, circle.y, stage[0].x, stage[0].y );
       endAngle = angleFrom( circle.x, circle.y, stage[1].x, stage[1].y );
-      ctx.arc( circle.x, circle.y, circle.radius, endAngle, startAngle, true );
+      // if ( startAngle > endAngle ) {
+      //   var temp = startAngle;
+      //   startAngle = endAngle;
+      //   endAngle = temp;
+      // }
+      ctx.arc( circle.x, circle.y, circle.radius, endAngle, startAngle, startAngle < endAngle );
 
       // ctx.strokeStyle = 'white';
       // ctx.lineWidth = 10;
       // ctx.stroke();
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+      // ctx.fillStyle = 'rgba(0, 0, 255, 0.6)';
       ctx.fill();
+
+      ctx.font = '12px Monaco';
+      ctx.fillStyle = 'white';
+      ctx.fillText( (startAngle * 1 / DEG_TO_RAD).toFixed(2), stage[0].x + 20, stage[0].y );
+      ctx.fillStyle = 'rgb(0, 255, 0)';
+      ctx.fillText( (endAngle * 1 / DEG_TO_RAD).toFixed(2), stage[1].x + 20, stage[1].y );
     }
 
     // console.log(circle.x, circle.y, circle.radius)
@@ -158,8 +306,8 @@ $(function() {
         dx20 = x2 - x0,
         dy20 = y2 - y0;
 
-    if ( dx20 === 0 || dy20 === 0 ) {
-      if ( dx10 === 0 || dy10 === 0 ) {
+    if ( !dx20 || !dy20 ) {
+      if ( !dx10 || !dy10 ) {
         // Don't do anything if degenerate.
         return;
       }
@@ -193,7 +341,7 @@ $(function() {
     var xden = 2 * ( dx10 - rdy10dy20 * dx20 ),
         yden = 2 * ( dy10 - rdx10dx20 * dy20 );
 
-    if ( xden === 0 || yden === 0 ) {
+    if ( !xden || !yden ) {
       return;
     }
 
@@ -218,6 +366,66 @@ $(function() {
     return Math.atan2( y1 - y0, x1 - x0 );
   }
 
+  /**
+   * Given the point (x, y), find the closest point on the line given by
+   * (x0, y0) -> (x1, y1).
+   */
+  function closestPointOnLine( x, y, x0, y0, x1, y1 ) {
+    var dx = x1 - x0,
+        dy = y1 - y0;
+
+    // Check for line degeneracy.
+    if ( !dx && !dy ) {
+      return;
+    }
+
+    var lengthSquared = dx * dx + dy * dy;
+
+    // Calculate parameter of closest point on line segment.
+    var t = ( ( x - x0 ) * ( x1 - x0 ) + ( y - y0 ) * ( y1 - y0 ) ) / lengthSquared;
+
+    return {
+      x: x0 + t * ( x1 - x0 ),
+      y: y0 + t * ( y1 - y0 )
+    };
+  }
+
+  /**
+   * Given the point (x, y), find the closest point on the circle defined by a
+   * center point (cx, cy) and radius.
+   */
+  function closestPointOnCircle( x, y, cx, cy, radius ) {
+    var angle = angleFrom( cx, cy, x, y );
+    return {
+      x: cx + Math.cos( angle ) * radius,
+      y: cy + Math.sin( angle ) * radius
+    };
+  }
+
+  /**
+   * Returns the perpendicular line segment to the line segment given by
+   * (x0, y0) and (x1, y1), The two line segments form the diagonals of a
+   * square.
+   *
+   * The (x0, y0) of the resulting line segment will be counterclockwise from
+   * the (x0, y0) of the original line segment.
+   */
+  function perpendicular( x0, y0, x1, y1 ) {
+    var dx = x1 - x0,
+        dy = y1 - y0;
+
+    // Midpoint.
+    var mx = x0 + 0.5 * dx,
+        my = y0 + 0.5 * dy;
+
+    return {
+      x0: mx - 0.5 * dy,
+      y0: my + 0.5 * dx,
+      x1: mx + 0.5 * dy,
+      y1: my - 0.5 * dx
+    };
+  }
+
   var $canvas = $( '#canvas' );
   var canvas = $canvas[0];
   var context = canvas.getContext( '2d' );
@@ -239,17 +447,17 @@ $(function() {
   };
 
   stage.push(
-    new Rect(
+    new Handler(
       bgCircle.x + Math.cos( 300 * DEG_TO_RAD ) * bgCircle.radius,
       bgCircle.y + Math.sin( 300 * DEG_TO_RAD ) * bgCircle.radius,
-      10, 10
+      10
     ),
-    new Rect(
+    new Handler(
       bgCircle.x + Math.cos( 120 * DEG_TO_RAD ) * bgCircle.radius,
       bgCircle.y + Math.sin( 120 * DEG_TO_RAD ) * bgCircle.radius,
-      10, 10
+      10
     ),
-    new Rect( bgCircle.x + 50, bgCircle.y + 50, 10, 10 )
+    new Handler( bgCircle.x + 50, bgCircle.y + 50, 10 )
   );
 
   tick();
