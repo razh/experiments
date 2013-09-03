44b963b3f5c17b38082ba24578fdc43a7991dd6d
Raymond Zhou <somecrazyidiot@gmail.com>
2013-09-03 16:48:56 -0400

    Clean up bg-gradient grid test.

--- background-gradient/js/grid-test.js
+++ background-gradient/js/grid-test.js
@@ -2,22 +2,8 @@
 $(function() {
   'use strict';
 
-  var bg00 = new Background(),
-      bg01 = new Background(),
-      bg02 = new Background(),
-      bg03 = new Background(),
-      bg04 = new Background(),
-      bg05 = new Background(),
-      bg06 = new Background(),
-      bg07 = new Background(),
-      bg08 = new Background(),
-      bg09 = new Background(),
-      bg10 = new Background(),
-      bg11 = new Background(),
-      bg12 = new Background(),
-      bg13 = new Background(),
-      bg14 = new Background(),
-      bg15 = new Background();
+  var backgroundCount = 16,
+      backgrounds = [];
 
   function randomGradient() {
     var grad = new LinearGradient();
@@ -27,12 +13,14 @@ $(function() {
       grad.angle = randomInt( 0, 359 ) + 'deg';
     }
 
-    var colors = [ 0, 128, 255 ];
+    var colors = [ 0, 128, 255 ],
+        maxColorIndex = colors.length - 1;
+
     for ( var i = 0; i < colorStopCount; i++ ) {
       grad.colorStops.push( new ColorStop( new RGBAColor(
-        colors[ Math.round( Math.random() * 2 ) ],
-        colors[ Math.round( Math.random() * 2 ) ],
-        colors[ Math.round( Math.random() * 2 ) ],
+        colors[ Math.round( Math.random() * maxColorIndex ) ],
+        colors[ Math.round( Math.random() * maxColorIndex ) ],
+        colors[ Math.round( Math.random() * maxColorIndex ) ],
         // randomInt( 0, 255 ),
         // randomInt( 0, 255 ),
         // randomInt( 0, 255 ),
@@ -50,41 +38,21 @@ $(function() {
     }
   }
 
-  populateBackground( bg00 );
-  populateBackground( bg01 );
-  populateBackground( bg02 );
-  populateBackground( bg03 );
-  populateBackground( bg04 );
-  populateBackground( bg05 );
-  populateBackground( bg06 );
-  populateBackground( bg07 );
-  populateBackground( bg08 );
-  populateBackground( bg09 );
-  populateBackground( bg10 );
-  populateBackground( bg11 );
-  populateBackground( bg12 );
-  populateBackground( bg13 );
-  populateBackground( bg14 );
-  populateBackground( bg15 );
+  // Initialize backgrounds.
+  var i;
+  for ( i = 0; i < backgroundCount; i++ ) {
+    backgrounds.push( new Background() );
+  }
 
-  $( '#gradient-00' ).css({ 'background-image': bg00.css() });
-  $( '#gradient-01' ).css({ 'background-image': bg01.css() });
-  $( '#gradient-02' ).css({ 'background-image': bg02.css() });
-  $( '#gradient-03' ).css({ 'background-image': bg03.css() });
-  $( '#gradient-04' ).css({ 'background-image': bg04.css() });
-  $( '#gradient-05' ).css({ 'background-image': bg05.css() });
-  $( '#gradient-06' ).css({ 'background-image': bg06.css() });
-  $( '#gradient-07' ).css({ 'background-image': bg07.css() });
-  $( '#gradient-08' ).css({ 'background-image': bg08.css() });
-  $( '#gradient-09' ).css({ 'background-image': bg09.css() });
-  $( '#gradient-10' ).css({ 'background-image': bg10.css() });
-  $( '#gradient-11' ).css({ 'background-image': bg11.css() });
-  $( '#gradient-12' ).css({ 'background-image': bg12.css() });
-  $( '#gradient-13' ).css({ 'background-image': bg13.css() });
-  $( '#gradient-14' ).css({ 'background-image': bg14.css() });
-  $( '#gradient-15' ).css({ 'background-image': bg15.css() });
+  var indexString;
+  for ( i = 0; i < backgroundCount; i++ ) {
+    populateBackground( backgrounds[i] );
+    indexString = i.toString();
+    indexString = ( indexString.length < 2 ) ? '0' + indexString : indexString;
+    $( '#gradient-' + indexString ).css( 'background-image', backgrounds[i].css() );
+  }
 
-  console.log( bg00.css() );
+  console.log( backgrounds[0].css() );
   console.log( $( '#gradient-00' ).css( 'background-image' ) );
 
   var $window = $( window );
@@ -101,6 +69,8 @@ $(function() {
   var cellWidth = 0.25 * $gradientContainer.width(),
       cellHeight = 0.25 * $gradientContainer.height();
 
+  var duration = 200;
+
   $gradients.each(function( index, gradient ) {
     var $gradient = $( gradient );
 
@@ -109,36 +79,34 @@ $(function() {
       height: cellHeight
     });
 
-    $gradient.on( 'click', clickOn );
+    $gradient.on( 'click', function() {
+      console.log( $gradient.css( 'background-image' ) );
+
+      $gradientFullscreen.css({
+        'background-image':  $gradient.css( 'background-image' ),
+        'z-index': 1
+      });
+
+      $gradients.animate({
+        opacity: 0
+      });
+
+      $gradientFullscreen.animate({
+        opacity: 1
+      }, duration, 'swing', function() {
+        $gradientFullscreen.on( 'click', clickOut );
+      });
+    });
   });
-
-  function clickOn() {
-    var $this = $( this );
-
-    $gradientFullscreen.css({
-      'background-image':  $this.css( 'background-image' ),
-      'z-index': 1
-    });
-
-    $gradients.animate({
-      opacity: 0
-    });
-
-    $gradientFullscreen.animate({
-      opacity: 1
-    }, 200, 'swing', function() {
-      $gradientFullscreen.on( 'click', clickOut );
-    });
-  }
 
   function clickOut() {
     $gradients.animate({
       opacity: 1
-    }, 200 );
+    }, duration );
 
     $gradientFullscreen.animate({
       opacity: 0
-    }, 200, 'swing', function() {
+    }, duration, 'swing', function() {
       $gradientFullscreen.css({
         'background-image': '',
         'z-index': -1
