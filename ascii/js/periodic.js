03d42d0a746670f4cef913acf8aebf1a33acf46c
Raymond Zhou <somecrazyidiot@gmail.com>
2013-09-17 01:09:05 -0400

    ascii[periodic]: Move logo drawing to separate canvas, allowing for better diff. Convert to px units only.

--- ascii/js/periodic.js
+++ ascii/js/periodic.js
@@ -29,66 +29,76 @@
   var diffCanvas  = document.getElementById( 'diff-canvas' ),
       diffContext = diffCanvas.getContext( '2d' );
 
+  var logoCanvas = document.createElement( 'canvas' ),
+      logoContext = logoCanvas.getContext( '2d' );
+
   var image = document.getElementById( 'periodic-img' );
 
   var WIDTH  = image.width  = normalCanvas.width  = diffCanvas.width  = 256,
       HEIGHT = image.height = normalCanvas.height = diffCanvas.height = 256;
 
-  function drawNormal( ctx ) {
-    drawLogo( ctx );
+  logoCanvas.width = WIDTH;
+  logoCanvas.height = HEIGHT;
+
+  function drawNormal( ctx, symbol, options ) {
+    drawLogo( logoContext, symbol, options );
+    ctx.drawImage( logoCanvas, 0, 0, WIDTH, HEIGHT );
   }
 
-  function drawDiff( ctx ) {
+  function drawDiff( ctx, symbol, options ) {
     ctx.globalCompositeOperation = 'normal';
 
     ctx.clearRect( 0, 0, WIDTH, HEIGHT );
     ctx.drawImage( image, 0, 0, WIDTH, HEIGHT );
 
     ctx.globalCompositeOperation = 'difference';
-    drawLogo( ctx );
+
+    drawLogo( logoContext, symbol, options );
+    ctx.drawImage( logoCanvas, 0, 0, WIDTH, HEIGHT );
   }
 
-  function drawLogo( ctx ) {
+  function drawLogo( ctx, symbol, options ) {
     ctx.beginPath();
-    ctx.rect( 3, 3, WIDTH - 6, HEIGHT - 6 );
+    ctx.rect( 4, 4, WIDTH - 8, HEIGHT - 8 );
 
-    ctx.lineWidth = 6;
+    ctx.lineWidth = 8;
     ctx.strokeStyle = 'white';
     ctx.stroke();
 
     var grad = ctx.createLinearGradient( 0, 0, WIDTH, HEIGHT );
-    grad.addColorStop( 0, '#6F9969' );
-    grad.addColorStop( 1, '#202E11' );
+    grad.addColorStop( 0, 'rgb(121, 184, 124)' );
+    grad.addColorStop( 1, 'rgb(25, 38, 6)' );
 
     ctx.fillStyle = grad;
     ctx.fill();
 
-    var br = elements.filter(function( element ) {
-      return element.symbol === 'Br';
+    var el = elements.filter(function( element ) {
+      return element.symbol === symbol;
     })[0];
 
     // Draw symbol.
-    ctx.font = 'bold 110pt Helvetica';
+    ctx.font = 'bold 130px Helvetica'; // Smaller font for wide symbols.
+    ctx.font = 'bold 150px Helvetica';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    ctx.fillStyle = '#FCFBFF';
-    ctx.fillText( br.symbol, 0.5 * WIDTH, 0.5 * HEIGHT - 3 );
+    ctx.fillStyle = '#fff';
+    ctx.fillText( el.symbol, 0.5 * WIDTH, 0.5 * HEIGHT - 3 );
 
     ctx.textAlign = 'left';
 
     // Draw atomic mass.
-    ctx.font = '16pt Helvetica';
-    ctx.fillText( br.atomicMass, 18, 30 );
+    ctx.font = '21px Helvetica';
+    ctx.fillText( el.atomicMass, 18, 30 );
     // Draw atomic number.
-    ctx.font = '23pt Helvetica';
-    ctx.fillText( br.atomicNumber, 13, 196 );
+    ctx.font = '31px Helvetica';
+    ctx.fillText( el.atomicNumber, 13, 195 );
     // Draw electron configuration.
-    ctx.font = '15pt Helvetica';
-    ctx.fillText( br.electronConfiguration, 15, 230 );
+    ctx.font = '20px Helvetica';
+    ctx.fillText( el.electronConfiguration, 15, 230 );
     // Draw oxidation states.
-    ctx.font = '16pt Helvetica';
+    ctx.font = '21px Helvetica';
     ctx.textAlign = 'right';
-    br.oxidationStates.forEach(function( state, index ) {
+    el.oxidationStates.forEach(function( state, index ) {
       ctx.fillText( state, 246, 30 + index * 25 );
     });
   }
@@ -103,11 +113,16 @@
     xhr.onreadystatechange = function() {
       if ( this.readyState === 4 && this.status === 200 ) {
         elements = JSON.parse( this.responseText );
-        drawNormal( normalContext );
-        drawDiff( diffContext );
+
+        var symbol = 'Br';
+        drawNormal( normalContext, symbol );
+        drawDiff( diffContext, symbol );
 
         console.log( 'norm: ' + calculateBrightness( normalContext ) );
         console.log( 'diff: ' + calculateBrightness( diffContext ) );
+
+        var current = 12.7373641815189;
+        console.log( current - calculateBrightness( diffContext ) );
       }
     };
 
