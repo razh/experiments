f83e66e2fb0118239dbd04d7a416bed18e4e3278
Raymond Zhou <somecrazyidiot@gmail.com>
2013-09-21 22:22:27 -0400

    text-width: Replace linear font-size determination algorithm with binary search.

--- text-width/js/main.js
+++ text-width/js/main.js
@@ -23,18 +23,63 @@ $(function() {
     };
   }) ();
 
-  var editorText = $editor.html();
-
   function toTextArray( text ) {
     return text.replace( /<div><br><\/div>/gi, '' ) // Remove all line breaks.
       .replace( /<div>/gi, '\n' ) // Add new lines where at div start.
       .replace( /<\/div>/gi, '' ) // Remove closing div tag.
+      .replace( /&nbsp;/g, '' ) // Remove nbsps.
       .split( '\n' );
   }
 
+  var editorText = '';
   var editorTextArray = toTextArray( editorText );
 
-  $editor.on('input', function() {
+  function updatePreview( textArray ) {
+    $preview.empty();
+
+    var previewWidth = $preview.width();
+
+    textArray.forEach(function( text ) {
+      // Binary search.
+      var low = 0;
+      var high = 512;
+      var fontSize = high;
+      var currentWidth;
+
+      while ( low <= high ) {
+        fontSize = Math.round( 0.5 * ( low + high ) );
+        currentWidth = textWidth( text, 'bold ' + fontSize + 'px Helvetica' );
+        if ( currentWidth < previewWidth ) {
+          low = fontSize + 1;
+        } else if ( currentWidth > previewWidth ) {
+          high = fontSize - 1;
+        } else {
+          break;
+        }
+      }
+
+      if ( currentWidth > previewWidth ) {
+        fontSize--;
+      }
+
+      if ( currentWidth !== previewWidth ) {
+        low = textWidth( text, 'bold ' + (fontSize - 1) + 'px Helvetica' );
+        high = textWidth( text, 'bold ' + (fontSize + 1) + 'px Helvetica' );
+        console.log(currentWidth + ', ' + previewWidth + ', [' + low + ', ' + high + ']');
+      }
+
+      var $line = $( '<div>' + text + '</div>' );
+      $line.css({
+        'font-weight': 'bold',
+        'font-size': fontSize + 'px',
+        'font-family': 'Helvetica'
+      });
+
+      $preview.append( $line );
+    });
+  }
+
+  function update() {
     var text = $editor.html();
     if ( editorText !== text ) {
       editorText = text;
@@ -43,25 +88,8 @@ $(function() {
 
       updatePreview( editorTextArray );
     }
-  });
-
-  function updatePreview( textArray ) {
-    $preview.empty();
-
-    var previewWidth = $preview.width() - 20;
-
-    textArray.forEach(function( text ) {
-      // Suboptimal linear search for correct size.
-      var fontSize = 128;
-
-      while ( previewWidth < textWidth( text, fontSize + 'px Helvetica' ) ) {
-        fontSize--;
-      }
-
-      var $line = $( '<div>' + text + '</div>' );
-      $line.css( 'font', fontSize + 'px Helvetica' );
-
-      $preview.append( $line );
-    });
   }
+
+  update();
+  $editor.on('input', update );
 });
