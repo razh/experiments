3033f88197a7b49db1c987cea32dcdeea29c1079
Raymond Zhou <somecrazyidiot@gmail.com>
2013-09-27 15:50:38 -0400

    illustrations[hilbert-curve]: Add 3D Hilbert curve.

--- illustrations/js/hilbert-curve.js
+++ illustrations/js/hilbert-curve.js
@@ -11,9 +11,6 @@
    * Thomas Diewald's Hilbert curve algorithm.
    * http://www.openprocessing.org/sketch/15493
    */
-  var signX = [ -1, +1, +1, -1 ],
-      signY = [ -1, -1, +1, +1 ];
-
   function Hilbert2D( options ) {
     this.size  = options.size  || 0;
     this.depth = options.depth || 0;
@@ -30,13 +27,18 @@
       this.dy[i] = [];
 
       for ( j = 0; j < 4; j++ ) {
-        this.dx[i][j] = signX[j] * size;
-        this.dy[i][j] = signY[j] * size;
+        this.dx[i][j] = Hilbert2D.sign.x[j] * size;
+        this.dy[i][j] = Hilbert2D.sign.y[j] * size;
       }
     }
 
     this.generate( 0, 0, this.depth, 0, 1, 2, 3 );
   }
+
+  Hilbert2D.sign = {
+    x: [ -1, +1, +1, -1 ],
+    y: [ -1, -1, +1, +1 ]
+  };
 
   Hilbert2D.prototype.draw = function( ctx ) {
     if ( !this.vertices.length ) {
@@ -120,5 +122,63 @@
   }
 
   draw( context );
+
+
+  /**
+   * http://www.openprocessing.org/visuals/?visualID=15599
+   */
+  function Hilbert3D( options ) {
+    this.size  = options.size  || 0;
+    this.depth = options.depth || 0;
+
+    this.vertices = [];
+
+    this.dx = [];
+    this.dy = [];
+    this.dz = [];
+
+    var size = 0.5 * this.size;
+    var i, j;
+    for ( i = this.depth - 1; i >= 0; i--, size *= 0.5 ) {
+      this.dx[i] = [];
+      this.dy[i] = [];
+      this.dz[i] = [];
+
+      for ( j = 0; j < 8; j++ ) {
+        this.dx[i][j] = Hilbert2D.sign.x[j] * size;
+        this.dy[i][j] = Hilbert2D.sign.y[j] * size;
+        this.dz[i][j] = Hilbert2D.sign.z[j] * size;
+      }
+    }
+
+    this.generate( 0, 0, 0, this.depth, 0, 1, 2, 3, 4,5, 6, 7 );
+  }
+
+  Hilbert3D.sign = {
+    x: [ -1, -1, -1, -1, +1, +1, +1, +1 ],
+    y: [ +1, +1, -1, -1, -1, -1, +1, +1 ],
+    z: [ -1, +1, +1, -1, -1, +1, +1, -1 ]
+  };
+
+  Hilbert3D.prototype.generate = function( x, y, z, depth, a, b, c, d, e, f, g, h ) {
+    if ( !depth ) {
+      this.vertices[ this.vertices.length ] = [ x, y, z ];
+    } else {
+      depth--;
+
+      var dx = this.dx[ depth ],
+          dy = this.dy[ depth ],
+          dz = this.dz[ depth ];
+
+      this.generate( x + dx[a], y + dy[a], z + dz[a], depth, a, d, e, h, g, f, c, b );
+      this.generate( x + dx[b], y + dy[b], z + dz[b], depth, a, h, g, b, c, f, e, d );
+      this.generate( x + dx[c], y + dy[c], z + dz[c], depth, a, h, g, b, c, f, e, d );
+      this.generate( x + dx[d], y + dy[d], z + dz[d], depth, c, d, a, b, g, h, e, f );
+      this.generate( x + dx[e], y + dy[e], z + dz[e], depth, c, d, a, b, g, h, e, f );
+      this.generate( x + dx[f], y + dy[f], z + dz[f], depth, e, d, c, f, g, b, a, h );
+      this.generate( x + dx[g], y + dy[g], z + dz[g], depth, e, d, c, f, g, b, a, h );
+      this.generate( x + dx[h], y + dy[h], z + dz[h], depth, g, f, c, b, a, d, e, h );
+    }
+  };
 
 }) ( window, document );
