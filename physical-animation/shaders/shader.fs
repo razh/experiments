c976a2e0fe3b6a103b7cb6b526deaebd4fe87747
Raymond Zhou <somecrazyidiot@gmail.com>
2013-09-30 16:57:44 -0400

    physical-animation: Initial commit with working CSS shaders.

---
+++ physical-animation/shaders/shader.fs
@@ -0,0 +1,8 @@
+precision mediump float;
+
+void main() {
+  css_ColorMatrix = mat4(1.0, 0.0, 0.0, 0.0,
+                         0.0, 1.0, 0.0, 0.0,
+                         0.0, 0.0, 1.0, 0.0,
+                         0.0, 0.0, 0.0, 1.0);
+}
