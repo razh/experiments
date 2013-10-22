c0ccea02ce5f0a7dceefee8dd62eb8202111859b
Raymond Zhou <somecrazyidiot@gmail.com>
2013-10-22 02:29:41 -0400

    illustrations[top-down-city]: Test multiple buildings/shadows. Add basic touch support.

--- illustrations/js/top-down-city.js
+++ illustrations/js/top-down-city.js
@@ -2,46 +2,71 @@
   'use strict';
 
   var shadowColor = '#555',
-      shadowBlur  = '0',
-      shadowCount = 64;
+      shadowBlur  = 0,
+      shadowCount = 4;
 
-  var mouse = {
-    x: 0,
-    y: 0
-  };
+  var shadows   = [].slice.call( document.getElementsByClassName( 'shadow' ) ),
+      buildings = [].slice.call( document.getElementsByClassName( 'building' ) );
 
-  var building = document.getElementsByClassName( 'building' )[0],
+  (function() {
+    var computedStyle = window.getComputedStyle( buildings[0] );
+    console.log( computedStyle.width, computedStyle.height );
+  }) ();
 
-    buildingWidth  = building.clientWidth,
-    buildingHeight = building.clientHeight,
+  // Push all shadows underneath the buildings.
+  function resize() {
+    var buildingsEl = document.getElementsByClassName( 'buildings' )[0];
 
-    halfWidth  = 0.5 * buildingWidth,
-    halfHeight = 0.5 * buildingHeight;
+    // Get buildings container offset.
+    var buildingsTop  = buildingsEl.offsetTop,
+        buildingsLeft = buildingsEl.offsetLeft;
 
-  var computedStyle = window.getComputedStyle( building );
-  console.log( computedStyle.width, computedStyle.height );
+    shadows.forEach(function( shadow, index ) {
+      var building = buildings[ index ];
 
-  function onMouseMove( event ) {
-    // Coordinates from center of building.
-    mouse.x = event.pageX - building.offsetLeft - halfWidth;
-    mouse.y = event.pageY - building.offsetTop  - halfHeight;
-
-    building.style.boxShadow = generateCSS( mouse.x, mouse.y );
+      shadow.style.top  = building.offsetTop  + buildingsTop + 'px';
+      shadow.style.left = building.offsetLeft + buildingsLeft + 'px';
+    });
   }
 
-  function generateCSS( x, y ) {
+  function onMouseMoveAll( event ) {
+    event.preventDefault();
+
+    var computedStyle;
+    var width, height;
+    var halfSize;
+    var x, y;
+    shadows.forEach(function( shadow ) {
+      computedStyle = window.getComputedStyle( shadow );
+
+      width  = parseInt( computedStyle.width,  10 );
+      height = parseInt( computedStyle.height, 10 );
+
+      halfSize = 0.5 * Math.max( width, height );
+      x = event.pageX - shadow.offsetLeft - halfSize;
+      y = event.pageY - shadow.offsetTop  - halfSize;
+
+      shadow.style.boxShadow = generateCSS( x, y, halfSize );
+    });
+  }
+
+  function generateCSS( x, y, halfSize ) {
     var css = '';
 
     var length = Math.sqrt( x * x + y * y );
 
-    var dx = -x / length * 16,
-        dy = -y / length * 16;
+    var dx = -x / length * 32,
+        dy = -y / length * 32;
 
+    var shadowSpread;
     for ( var i = 0; i < shadowCount; i++ ) {
-      css += Math.round( i * dx ) + 'px ' +
-        Math.round( i * dy ) + 'px ' +
-        shadowBlur + ' ' +
-        Math.round( -( i / shadowCount ) * halfWidth ) + 'px ' +
+      // Increase shadow spread up to 1.25 of building size (0.5 * halfSize).
+      shadowSpread = Math.round( 0.5 * ( i / shadowCount ) * halfSize );
+
+      css += Math.round( ( i + 1 ) * dx ) + 'px ' +
+        Math.round( ( i + 1 ) * dy ) + 'px ' +
+        shadowBlur + 'px ' +
+        shadowSpread + 'px ' +
         shadowColor;
 
       if ( i < shadowCount - 1 ) {
@@ -52,19 +77,22 @@
     return css;
   }
 
-  onMouseMove({ pageX: 0, pageY: 0 });
-  document.addEventListener( 'mousemove', onMouseMove );
-
+  resize();
+  onMouseMoveAll({ pageX: 0, pageY: 0, preventDefault: function() {} });
+  document.addEventListener( 'mousemove', onMouseMoveAll );
+  document.addEventListener( 'touchmove', onMouseMoveAll );
 
   // Remove all event listeners.
   function onKeyDown( event ) {
     // ESC.
     if ( event.which === 27 ) {
-      document.removeEventListener( 'mousemove', onMouseMove );
+      document.removeEventListener( 'mousemove', onMouseMoveAll );
+      document.removeEventListener( 'touchmove', onMouseMoveAll );
       document.removeEventListener( 'keydown', onKeyDown );
+      window.removeEventListener( 'resize', resize );
     }
-
   }
 
   document.addEventListener( 'keydown', onKeyDown );
+  window.addEventListener( 'resize', resize );
 }) ( window, document );
