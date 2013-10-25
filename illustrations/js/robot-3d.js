75db54c61b13d0ca07cf3a10b05865dd780d1c9c
Raymond Zhou <somecrazyidiot@gmail.com>
2013-10-25 19:00:31 -0400

    illustrations[body-3d]: Add a basic mouselook camera (spacebar toggles).

--- illustrations/js/robot-3d.js
+++ illustrations/js/robot-3d.js
@@ -1,6 +1,58 @@
 (function( window, document, undefined ) {
   'use strict';
 
+
+  var body = document.getElementsByClassName( 'body' )[0],
+      bodyTransition = window.getComputedStyle( body ).transition;
+
+  // Offset the transform origin so rotateY revolves around
+  // an axis outside of the body.
+  var transformOrigin = '50% 50% 0';
+  body.style.webkitTransformOrigin = transformOrigin;
+  body.style.transformOrigin = transformOrigin;
+
+  function onMouseMove( event ) {
+    var rx =   ( 0.5 - ( event.clientY / window.innerHeight ) ) * 180,
+        ry =  -( 0.5 - ( event.clientX / window.innerWidth  ) ) * 180;
+
+    var transform = 'rotateX( ' + rx + 'deg) rotateY( ' + ry + 'deg)';
+    body.style.webkitTransform = transform;
+    body.style.transform = transform;
+  }
+
+  onMouseMove({
+    pageX: 0.5 * window.innerWidth,
+    pageY: 0.5 * window.inenrHeight
+  });
+
+  // Toggle on/off the mouse-controlled rotating camera.
+  document.addEventListener( 'keydown', function( event ) {
+    // Space.
+    if ( event.which === 32 ) {
+      if ( window.getComputedStyle( body ).transitionProperty !== 'none' ) {
+        body.style.transition = 'none';
+
+        transformOrigin = '0 0 0';
+        body.style.webkitTransformOrigin = transformOrigin;
+        body.style.transformOrigin = transformOrigin;
+
+        document.addEventListener( 'mousemove', onMouseMove );
+      } else {
+        body.style.transition = bodyTransition;
+
+        // Reset transform and transform origin.
+        body.style.webkitTransform = '';
+        body.style.transform = '';
+
+        transformOrigin = '50% 50% 0';
+        body.style.webkitTransformOrigin = transformOrigin;
+        body.style.transformOrigin = transformOrigin;
+
+        document.removeEventListener( 'mousemove', onMouseMove );
+      }
+    }
+  });
+
   /**
    * Need to handle the nested skeleton structure.
    *
---
+++ illustrations/js/robot-3d/main.js
@@ -0,0 +1,25 @@
+/*globals requirejs, define*/
+requirejs.config({
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: [ 'jquery', 'underscore' ],
+      exports: 'Backbone'
+    }
+  },
+
+  paths: {
+    'backbone': '../../app/components/backbone/backbone-min',
+    'jquery': '../../app/components/jquery/jquery.min',
+    'underscore': '../../app/components/underscore/underscore-min',
+    'text': '../../app/components/requirejs-text/text'
+  }
+});
+
+define(function( require ) {
+  'use strict';
+
+  var Transform = require( 'views/transform-view' );
+});
