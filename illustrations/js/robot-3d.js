a488121af41447c1673c8089a89347bc7a4e3710
Raymond Zhou <somecrazyidiot@gmail.com>
2013-10-25 14:22:05 -0400

    illustrations[robot-3d]: List of things to do.

---
+++ illustrations/js/robot-3d.js
@@ -0,0 +1,28 @@
+(function( window, document, undefined ) {
+  'use strict';
+
+  /**
+   * Need to handle the nested skeleton structure.
+   *
+   * Each of these needs to be able to take a number of transform functions.
+   *  matrix()
+   *  matrix3d()
+   *  rotate()
+   *  rotate3d()
+   *  rotateX()
+   *  rotateY()
+   *  rotateZ()
+   *  scale()
+   *  scale3d()
+   *  scaleX()
+   *  scaleY()
+   *  scaleZ()
+   *  skewX()
+   *  skewY()
+   *  translate()
+   *  translate3d()
+   *  translateX()
+   *  translateY()
+   *  translateZ()
+   */
+}) ( window, document );
