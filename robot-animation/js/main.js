2928173938aa207eda2943bae115d28d00d0f09d
Raymond Zhou <somecrazyidiot@gmail.com>
2013-10-30 19:31:20 -0400

    robot-animation: Incomplete port of robot configuration data to JS.

--- robot-animation/js/main.js
+++ robot-animation/js/main.js
@@ -77,5 +77,123 @@ define(function( require ) {
 
   var robotTemplate = require( 'text!templates/robot.html' );
 
-  $( '.robot' ).html( _.template( robotTemplate ) );
+  var $robot = $( '.robot' );
+  $robot.html( _.template( robotTemplate ) );
+
+  // Haphazard configuration object.
+  var config = (function() {
+    var head = {
+      width: 40,
+      height: 40,
+      depth: 40
+    };
+
+    var chest = {
+      width: 80,
+      height: 100,
+      depth: 35
+    };
+
+    var hips = {
+      width: 60,
+      height: 40,
+      depth: 35
+    };
+
+    var upperArm = {
+      width: 20,
+      height: 60,
+      depth: 20
+    };
+
+    var lowerArm = {
+      width: 20,
+      height: 40,
+      depth: 20
+    };
+
+    var hand = {
+      width: 20,
+      height: 20,
+      depth: 20
+    };
+
+    // Note: the femur should be ~26% of body height.
+    var upeprLeg = {
+      width: 25,
+      height: 50,
+      depth: 25
+    };
+
+    // Femur:tibia ratio should be 56:44. This guy's proportions are out of wack.
+    var lowerLeg = {
+      width: 25,
+      height: 70,
+      depth: 25
+    };
+
+    var foot = {
+      width: 30,
+      height: 15,
+      depth: 50
+    };
+
+    return {
+      head: {
+        dimensions: [ head.width, head.height, head.depth ],
+        translate3d: [ 0, -80, 0 ],
+      },
+      chest: {
+        dimensions: [ chest.width, chest.height, chest.depth ]
+      },
+      hips: {
+        dimensions: [ hips.width, hips.height, hips.depth ],
+        translate3d: [ 0, 80, 0 ],
+        transformOrigin: [ 0, -0.5 * hips.height ]
+      },
+      upperArm: {
+        dimensions: [ upperArm.width, upperArm.height, upperArm.depth ],
+        transformOrigin: [ 0, -0.5 * upperArm.height ]
+      },
+      lowerArm: {
+        dimensions: [ lowerArm.width, lowerArm.height, lowerArm.depth ],
+        translate3d: [ 0, 60, 0 ],
+        transformOrigin: [ 0, -0.5 * lowerArm.height ]
+      },
+      hand: {
+        dimensions: [ hand.width, hand.height, hand.depth ],
+        translate3d: [ 0, 40, 0 ],
+        transformOrigin: [ 0, -0.5 * hand.height ]
+      },
+      armLeft: [ 60 ],
+      armRight: [ -60 ],
+      upperLeg: {
+        dimensions: [ 25, 50, 25 ],
+        translate3d: [ 0, 55, 0 ]
+      },
+      lowerLeg: {
+        dimensions: [ 25, 70, 25 ],
+        translate3d: [ 0, 70, 0 ]
+      },
+      foot: {
+        dimensions: [ 30, 15, 50 ],
+        translate3d: [ 0, 50, 10 ]
+      },
+      legLeft: [ 25 ],
+      legRight: [ -25 ]
+    };
+  }) ();
+
+  function createBoxView( name ) {
+    return new BoxView({
+      el: $robot.find( name ),
+      model: new Box( config[ name ].dimensions ),
+      transforms: new Transforms([
+        new Transform.Translate3D( config[ name ].translate3d )
+      ]),
+      transformOrigin: new Transform.Origin( config)
+    });
+  }
+
+  var headBoxView = createBoxView( 'head' );
 });
