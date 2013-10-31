7bde9abfc82d119ad57e77601cfb7a90e43c71d7
Raymond Zhou <somecrazyidiot@gmail.com>
2013-10-31 16:32:49 -0400

    robot-animation: Cleanup/remove old test code.

--- robot-animation/js/main.js
+++ robot-animation/js/main.js
@@ -31,50 +31,6 @@ define(function( require ) {
   var Box     = require( 'models/box' ),
       BoxView = require( 'views/box-view' );
 
-  var mat = new Transform.Matrix( [ 10, 20, 30, 40, 5, 200, 2000 ] );
-  console.log( mat.attributes );
-
-  var matView = new TransformView({
-    el: '#matrix',
-    model: mat
-  });
-
-  matView.render();
-
-  var boxView = new BoxView({
-    el: '#box',
-    model: new Box( [ 100, 50, 100 ] ),
-    transforms: new Transforms([
-      new Transform.RotateY( 40 ),
-      new Transform.RotateX( 30 )
-    ]),
-    transformOrigin: new Transform.Origin()
-  });
-
-  boxView.render();
-
-  var boxDimensionsView = new TransformView({
-    el: '#box-dimensions',
-    model: boxView.model
-  });
-
-  boxDimensionsView.render();
-
-
-  var rotateYView = new TransformView({
-    el: '#rotateY',
-    model: boxView.transforms.at(0)
-  });
-
-  rotateYView.render();
-
-  var rotateXView = new TransformView({
-    el: '#rotateX',
-    model: boxView.transforms.at(1)
-  });
-
-  rotateXView.render();
-
   var robotTemplate = require( 'text!templates/robot.html' );
 
   var $robot = $( '.robot' );
