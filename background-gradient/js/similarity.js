8884752dc6d9e5d9df289da2028d54734f18bdd0
Raymond Zhou <somecrazyidiot@gmail.com>
2013-11-06 01:35:34 -0500

    background-gradient[similarity]: Outline more tests/experiments.

--- background-gradient/js/similarity.js
+++ background-gradient/js/similarity.js
@@ -1,5 +1,20 @@
+/*globals LinearGradient, ColorStop, RGBAColor*/
 (function( window, document, undefined ) {
   'use strict';
+
+  /**
+   * Returns the canvas version of a linear gradient.
+   */
+  LinearGradient.prototype.canvas = function( ctx, x, y, width, height ) {
+
+    var gradient = ctx.createLinearGradient( x, y, width, height );
+
+    this.colorStops.forEach(function( colorStop ) {
+      console.log( colorStop.css() );
+    });
+
+    return gradient;
+  };
 
   // Syntax explorations.
   (function() {
@@ -20,5 +35,33 @@
 
     gradientCtx.fillStyle = grad;
     gradientCtx.fillRect( 0, 0, WIDTH, HEIGHT );
+
+
+
   }) ();
+
+  // Test converting between CSS and canvas linear gradients.
+  (function() {
+    var el = document.querySelector( '.conversion' );
+
+    var gradientCSS = el.querySelector( '.gradient-css' );
+
+    var gradientCanvas = el.querySelector( '.gradient-canvas' ),
+        gradientCtx    = gradientCanvas.getContext( '2d' );
+
+    var grad = new LinearGradient();
+
+    console.log( grad );
+
+    grad.colorStops = grad.colorStops.concat([
+      new ColorStop( new RGBAColor( 255, 255, 255, 1.0 ) ),
+      new ColorStop( new RGBAColor( 255, 255, 255, 1.0 ) )
+    ]);
+
+    grad.angle = '45deg';
+
+    console.log( grad.css() );
+  }) ();
+
+  // Test diff.
 }) ( window, document );
