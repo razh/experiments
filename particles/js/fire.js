fff3b89e91c61ad046403ada5dd89ad73de615a2
Raymond Zhou <somecrazyidiot@gmail.com>
2013-11-20 15:26:39 -0500

    particles[fire]: Rotate particle canvases. Basic fire effect recreated.

--- particles/js/fire.js
+++ particles/js/fire.js
@@ -9,7 +9,7 @@
   canvas.width  = 640;
   canvas.height = 480;
 
-  var rectWidth  = 200,
+  var rectWidth  = 100,
       rectHeight = 300;
 
   var leftParticles = [],
@@ -24,7 +24,7 @@
   }
 
   function radiusRandom() {
-    return Math.random() * 20 + 5;
+    return Math.random() * 30 + 5;
   }
 
   function vxRandom() {
@@ -47,7 +47,7 @@
 
   // Generate particles.
   (function() {
-    var particleCount = 80;
+    var particleCount = 150;
     while ( particleCount-- ) {
       leftParticles.push( randomParticle() );
       rightParticles.push( randomParticle());
@@ -60,6 +60,9 @@
 
     ctx.clearRect( 0, 0, width, height );
 
+    ctx.save();
+    ctx.translate( 0.25 * rectWidth, 0 );
+
     particles.forEach(function( particle ) {
       ctx.beginPath();
       ctx.arc( particle.x, particle.y, particle.radius, 0, 2 * Math.PI );
@@ -68,6 +71,16 @@
 
       particle.x += particle.vx;
       particle.y += particle.vy;
+      if ( particle.x - particle.radius < 0 ) {
+        particle.x = particle.radius;
+        particle.vx = -particle.vx;
+      }
+
+      if ( particle.x + particle.radius > rectWidth ) {
+        particle.x = rectWidth - particle.radius;
+        particle.vx = -particle.vx;
+      }
+
       if ( particle.y + particle.radius < 0 ) {
         particle.x = xRandom();
         particle.y = rectHeight + particle.radius;
@@ -75,6 +88,8 @@
         particle.vy = vyRandom();
       }
     });
+
+    ctx.restore();
   }
 
   function draw( ctx ) {
@@ -88,7 +103,7 @@
     var tempCanvas = document.createElement( 'canvas' ),
         tempCtx    = tempCanvas.getContext( '2d' );
 
-    tempCanvas.width = width;
+    tempCanvas.width  = width;
     tempCanvas.height = height;
 
     var gradient = tempCtx.createLinearGradient( 0, 0, 0, height );
@@ -106,13 +121,13 @@
     var leftCanvas = document.createElement( 'canvas' ),
         leftCtx    = leftCanvas.getContext( '2d' );
 
-    leftCanvas.width  = rectWidth;
+    leftCanvas.width  = 1.5 * rectWidth;
     leftCanvas.height = rectHeight;
 
     var rightCanvas = document.createElement( 'canvas' ),
         rightCtx    = rightCanvas.getContext( '2d' );
 
-    rightCanvas.width  = rectWidth;
+    rightCanvas.width  = 1.5 * rectWidth;
     rightCanvas.height = rectHeight;
 
     tempCtx.globalCompositeOperation = 'destination-out';
@@ -120,8 +135,29 @@
     drawParticles( leftCtx, leftParticles );
     drawParticles( rightCtx, rightParticles );
 
-    tempCtx.drawImage( leftCanvas, rectX - 0.75 * rectWidth, rectY );
-    tempCtx.drawImage( rightCanvas, rectX + 0.75 * rectWidth, rectY );
+    var leftX = rectX - rectWidth + 0.5 * leftCanvas.width;
+    var rightX = rectX + 0.5 * rectWidth + 0.5 * rightCanvas.width;
+    var imageY = rectY + 0.5 * rectHeight;
+
+    var angle = 10 * Math.PI / 180;
+    var scaleX = 1.3;
+    var scaleY = 1.5;
+
+    tempCtx.save();
+    tempCtx.translate( leftX, imageY );
+    tempCtx.rotate( angle );
+    tempCtx.scale( scaleX, scaleY );
+    tempCtx.translate( -leftX, -imageY );
+    tempCtx.drawImage( leftCanvas, rectX - rectWidth, rectY );
+    tempCtx.restore();
+
+    tempCtx.save();
+    tempCtx.translate( rightX, imageY );
+    tempCtx.rotate( -angle );
+    tempCtx.scale( scaleX, scaleY );
+    tempCtx.translate( -rightX, -imageY );
+    tempCtx.drawImage( rightCanvas, rectX + 0.5 * rectWidth, rectY );
+    tempCtx.restore();
 
     tempCtx.globalCompositeOperation = 'source-over';
 
