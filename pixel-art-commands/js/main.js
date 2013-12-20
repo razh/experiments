aae77f58908f506927233b7c9d61957e515166de
Raymond Zhou <somecrazyidiot@gmail.com>
2013-12-20 02:32:25 -0500

    pixel-art-commands: Basic functionality.

--- pixel-art-commands/js/main.js
+++ pixel-art-commands/js/main.js
@@ -5,15 +5,57 @@
   var canvas = document.createElement( 'canvas' ),
       ctx    = canvas.getContext( '2d' );
 
-  document.appendChild( canvas );
+  document.body.appendChild( canvas );
 
   function exportCommands( image ) {
-    canvas.width = image.width;
-    canvas.height = image.height;
+    var width = image.width;
+    var height = image.height;
+
+    canvas.width = width;
+    canvas.height = height;
 
     ctx.drawImage( image, 0, 0 );
 
-    var data = ctx.getImageData( 0, 0, canvas.width, canvas.height );
+    var data = ctx.getImageData( 0, 0, width, height ).data;
+
+    // Grab pixels.
+    var pixels = [];
+    var index;
+    var i, j;
+    var r, g, b;
+    for ( i = 0; i < height; i++ ) {
+      for ( j = 0; j < width; j++ ) {
+        index = 4 * ( i * width + j );
+
+        r = data[ index ];
+        g = data[ index + 1 ];
+        b = data[ index + 2 ];
+
+        pixels.push([
+          j, i,
+          r, g, b
+        ]);
+      }
+    }
+
+    var commands = [];
+    var pixel;
+    for ( i = 0; i < pixels.length; i++ ) {
+      pixel = pixels[i];
+
+      commands.push(
+        'fill(' +
+        pixel[2] + ', ' +
+        pixel[3] + ', ' +
+        pixel[4] + ');'
+      );
+
+      commands.push(
+        'rect(' + pixel[0] + ', ' + pixel[1] + ', 1, 1);'
+      );
+    }
+
+    document.getElementById( 'export' ).value = commands.join( '\n' );
   }
 
   document.addEventListener( 'drop', function( event ) {
