f2994bdaa2e2be0082831ca813f45f2db0fa44d2
Raymond Zhou <somecrazyidiot@gmail.com>
2013-12-27 00:00:48 -0500

    pixel-art-commands: Encapsulate everything in an object. Add a prerender function.
    Cannot scale image up by much after initialization.

--- pixel-art-commands/js/main.js
+++ pixel-art-commands/js/main.js
@@ -70,12 +70,33 @@
     });
 
     var scale = parseInt( scaleEl.value, 10 );
+    var name = nameEl.value || 'Entity';
     var spaces = '    ';
 
-    // To strings.
-    var commands = [
-      'var drawPixels = function(x, y) {',
-      spaces + 'noStroke();'
+    var constructorFn = [
+      'var ' + name + ' = function(x, y) {',
+      spaces + 'this.x = x || 0;',
+      spaces + 'this.y = y || 0;',
+      spaces + 'this.scaleX = 1;',
+      spaces + 'this.scaleY = 1;',
+      spaces + 'this.angle = 0;',
+      spaces + 'this.pixelSize = ' + scale + ';',
+      spaces + 'this.imageWidth = ' + image.width + ';',
+      spaces + 'this.imageHeight = ' + image.height + ';',
+      spaces + 'this.ctx = createGraphics(' +
+        'this.imageWidth * this.pixelSize, ' +
+        'this.imageHeight * this.pixelSize, ' +
+      'RGB);',
+      spaces + 'this.prerender();',
+      '};'
+    ];
+
+    var prerenderFn = [
+      name + '.prototype.prerender = function() {',
+      spaces + 'var ctx = this.ctx;',
+      spaces + 'ctx.beginDraw();',
+      spaces + 'ctx.noStroke();',
+      spaces + 'ctx.scale(this.pixelSize, this.pixelSize);',
     ];
 
     var pixel;
@@ -92,25 +113,41 @@
         g = pixel[3];
         b = pixel[4];
 
-        commands.push(
+        prerenderFn.push(
           spaces +
-          'fill(' +
+          'ctx.fill(' +
           r + ', ' +
           g + ', ' +
           b + ');'
         );
       }
 
-      commands.push(
+      prerenderFn.push(
         spaces +
-        'rect(' +
-        'x + ' + ( pixel[0] * scale ) + ', ' +
-        'y + ' + ( pixel[1] * scale ) + ', ' +
-        scale + ', ' + scale + ');'
+        'ctx.rect(' +
+        pixel[0] + ', ' +
+        pixel[1] + ', ' +
+        '1, 1);'
       );
     }
 
-    commands.push( '};' );
+    prerenderFn.push( spaces + 'ctx.endDraw();' );
+    prerenderFn.push( '};' );
+
+    var drawFn = [
+      name + '.prototype.draw = function() {',
+      spaces + 'pushMatrix();',
+      spaces + 'translate(this.x, this.y);',
+      spaces + 'rotate(this.angle);',
+      spaces + 'scale(this.scaleX, this.scaleY);',
+      spaces + 'image(this.ctx, 0, 0);',
+      spaces + 'popMatrix();',
+      '};'
+    ];
+
+    var commands = constructorFn
+      .concat( prerenderFn )
+      .concat( drawFn );
 
     document.getElementById( 'export' ).value = commands.join( '\n' );
   }
@@ -131,9 +168,15 @@
     event.preventDefault();
   });
 
-  var scaleEl = document.getElementById( 'scale' );
-  scaleEl.addEventListener( 'change', function() {
+
+  function exportImage() {
     exportCommands( image );
-  });
+  }
+
+  var scaleEl = document.getElementById( 'scale' );
+  scaleEl.addEventListener( 'change', exportImage );
+
+  var nameEl = document.getElementById( 'name' );
+  nameEl.addEventListener( 'change', exportImage );
 
 }) ( window, document );
