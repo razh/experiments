d597d2247535bfd106cd885b58c828c6d2913e98
Raymond Zhou <somecrazyidiot@gmail.com>
2014-02-06 02:39:40 -0500

    strategy[generals]: Add Entity, PhysicsEntity, Soldier classes.

--- strategy/js/generals.js
+++ strategy/js/generals.js
@@ -1,6 +1,75 @@
 (function( window, document, undefined ) {
   'use strict';
 
+  var PI2 = 2 * Math.PI;
+
+  function Entity( x, y ) {
+    this.x = x || 0;
+    this.y = y || 0;
+
+    this.angle = 0;
+  }
+
+  Entity.prototype.update = function() {};
+  Entity.prototype.draw = function() {};
+
+  function PhysicsEntity( x, y ) {
+    Entity.call( this, x, y );
+
+    // Velocity.
+    this.vx = 0;
+    this.vy = 0;
+
+    // Acceleration.
+    this.ax = 0;
+    this.ay = 0;
+
+    // Angular velocity.
+    this.va = 0;
+
+    // Previous position.
+    this.px = this.x;
+    this.py = this.y;
+  }
+
+  PhysicsEntity.prototype = Object.create( Entity.prototype );
+  PhysicsEntity.prototype.constructor = PhysicsEntity;
+
+  PhysicsEntity.prototype.update = function( dt ) {
+    this.px = this.x;
+    this.py = this.y;
+
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;
+
+    this.angle += this.va * dt;
+  };
+
+  function Soldier( x, y ) {
+    PhysicsEntity.call( this, x, y );
+  }
+
+  Soldier.prototype = Object.create( PhysicsEntity.prototype );
+  Soldier.prototype.constructor = Soldier;
+
+  Soldier.prototype.draw = function( ctx ) {
+    ctx.save();
+
+    ctx.translate( this.x, this.y );
+    ctx.rotate( -this.angle );
+
+    ctx.beginPath();
+    ctx.moveTo( 0, 0 );
+    ctx.arc( 0, 0, 5, 0, PI2 );
+    ctx.closePath();
+
+    ctx.restore();
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#fff';
+    ctx.stroke();
+  };
+
   function Game() {
     this.canvas = document.createElement( 'canvas' );
     this.ctx    = this.canvas.getContext( '2d' );
@@ -81,20 +150,47 @@
   }
 
   (function init() {
-    Game.instance = new Game();
-    document.body.appendChild( Game.instance.canvas );
+    var game = Game.instance = new Game();
+    document.body.appendChild( game.canvas );
 
     document.addEventListener( 'keydown', function( event ) {
       // Space.
-      if ( event.which === 32 ) {
+      if ( event.which === 32 && !game.running ) {
+        game.running = true;
         tick();
       }
 
       // ESC.
       if ( event.which === 27 ) {
-        Game.instance.running = false;
+        game.running = false;
       }
     });
+
+    var count = 300;
+    var soldier;
+    var x, y;
+    var speed, angle;
+    while ( count > 0 ) {
+      x = Math.round( Math.random() * window.innerWidth );
+      y = Math.round( Math.random() * window.innerHeight );
+      soldier = new Soldier( x, y );
+
+      // Set soldier properties.
+      speed = 50 + Math.random() * 50;
+      angle = Math.random() * PI2;
+      soldier.angle = angle;
+      soldier.vx = speed * Math.cos( -angle );
+      soldier.vy = speed * Math.sin( -angle );
+
+      game.add( soldier );
+      count--;
+    }
+
+    // Tick once.
+    console.time( 'tick' );
+    game.tick();
+    game.running = false;
+    console.timeEnd( 'tick' );
 
   }) ();
 }) ( window, document );
