ff59dec334c7014f6bd217617894187d7fe2c4c5
Raymond Zhou <somecrazyidiot@gmail.com>
2014-02-19 00:53:21 -0500

    spatial-grid: Draw cells intersecting with a bounding box.

--- spatial-grid/js/main.js
+++ spatial-grid/js/main.js
@@ -20,6 +20,23 @@ SpatialGrid*/
     }
   };
 
+  /**
+   * Draws the cells intersecting the given bounding box.
+   */
+  SpatialGrid.prototype.drawIntersection = function( ctx, x, y, width, height ) {
+    var xminIndex = this.xIndexOf( x ),
+        yminIndex = this.yIndexOf( y ),
+        xmaxIndex = this.xIndexOf( x + width ),
+        ymaxIndex = this.yIndexOf( y + height );
+
+    var xmin = xminIndex * this.cellWidth,
+        ymin = yminIndex * this.cellHeight,
+        xmax = ( xmaxIndex + 1 ) * this.cellWidth,
+        ymax = ( ymaxIndex + 1 ) * this.cellHeight;
+
+    ctx.rect( xmin, ymin, xmax - xmin, ymax - ymin );
+  };
+
   var potentials = [],
       actuals    = [];
 
@@ -58,6 +75,12 @@ SpatialGrid*/
       ctx.lineWidth = 1;
       ctx.strokeStyle = 'red';
       ctx.stroke();
+
+      // Intersection.
+      ctx.beginPath();
+      grid.drawIntersection( ctx, rect.x, rect.y, rect.width, rect.height );
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.25)';
+      ctx.fill();
     }
 
     drawRect( ctx );
