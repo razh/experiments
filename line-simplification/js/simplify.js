9a93a481dd4acfb67416185fdb12fa0df68daeb8
Raymond Zhou <somecrazyidiot@gmail.com>
2014-02-22 23:14:11 -0500

    line-simplification: Add MinHeap implementation.
    Untested. Need to switch to a 0-index implementation. Does not have a
    comparison function.

--- line-simplification/js/simplify.js
+++ line-simplification/js/simplify.js
@@ -13,19 +13,73 @@ var simplify = (function() {
   }
 
   MinHeap.prototype.push = function() {
+    for ( var i = 0, il = arguments.length; i < il; i++ ) {
+      this.up( this.array.push( arguments[i] ) - 1 );
+    }
 
+    return this.array.length;
   };
 
   MinHeap.prototype.pop = function() {
+    var array = this.array;
 
+    var min = array[1];
+    array[1] = array[ array.length - 1 ];
+    array.pop();
+
+    if ( array.length ) {
+      this.down(1);
+    }
+
+    return min;
   };
 
-  MinHeap.prototype.up = function() {
+  MinHeap.prototype.down = function( index ) {
+    var array  = this.array,
+        length = array.length - 1;
 
+    var leftIndex  = this.left( index ),
+        rightIndex = this.right( index );
+
+    var minIndex = index;
+
+    if ( leftIndex <= length && array[ leftIndex ] > array[ index ] ) {
+      minIndex = leftIndex;
+    }
+
+    if ( rightIndex <= length && array[ rightIndex ] > array[ minIndex ] ) {
+      minIndex = rightIndex;
+    }
+
+    var temp;
+    if ( minIndex !== index ) {
+      // Swap minIndex and this.
+      temp = array[ minIndex ];
+      array[ minIndex ] = array[ index ];
+      array[ index ] = temp;
+      this.down( minIndex );
+    }
   };
 
-  MinHeap.prototype.down = function() {
+  MinHeap.prototype.up = function( index ) {
+    var array  = this.array;
 
+    var parentIndex;
+    var temp;
+    while( index > 1 ) {
+      parentIndex = this.parent( index );
+      if ( array[ parentIndex] >= array[index ] )  {
+        break;
+      }
+
+      //  Swap parent and this.
+      temp = array[ index ];
+      array[ index ] = array[ parentIndex ];
+      array[ parentIndex ] = temp;
+
+      // Move up the tree.
+      index = parentIndex;
+    }
   };
 
   MinHeap.prototype.parent = function( index ) {
