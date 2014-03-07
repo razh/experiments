fc2d3ce0a9cd1b477e86704da53fa5cb4f1a86ba
Raymond Zhou <somecrazyidiot@gmail.com>
2014-03-07 01:40:11 -0500

    palette[rects]: Basic CSS animation test of expanding rects.

--- palette/js/rects.js
+++ palette/js/rects.js
@@ -1,3 +1,10 @@
 (function( window, document, undefined ) {
   'use strict';
-});
+
+  function lerp( a, b, t ) {
+    return a + t * ( b - a );
+  }
+
+  function addRect( el ) {
+  }
+}) ( window, document );
