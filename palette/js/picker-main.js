ac05b52e7fbaeef5d6e09d5552da0f4a4fe17e7f
Raymond Zhou <somecrazyidiot@gmail.com>
2014-03-09 03:02:12 -0400

    palette[picker]: Use mouse wheel to control HSL lightness.

--- palette/js/picker-main.js
+++ palette/js/picker-main.js
@@ -1,18 +1,19 @@
 (function( window, document, undefined ) {
   'use strict';
 
+  function clamp( value, min, max ) {
+    return Math.min( Math.max( value, min ), max );
+  }
+
   var hslEl = document.createElement( 'div' );
   hslEl.classList.add( 'hsl' );
   document.body.appendChild( hslEl );
 
-  window.addEventListener( 'mousemove', function( event ) {
-    var x = event.pageX,
-        y = event.pageY;
+  var h = 0,
+      s = 50,
+      l = 50;
 
-    var h = x / window.innerWidth * 360,
-        s = ( window.innerHeight - y ) / window.innerHeight * 100,
-        l = 50;
-
+  function update() {
     var hsl = 'hsl(' +
       Math.round( h ) + ', ' +
       Math.round( s ) + '%, ' +
@@ -20,5 +21,23 @@
 
     document.body.style.backgroundColor = hsl;
     hslEl.textContent = hsl;
+  }
+
+  window.addEventListener( 'mousemove', function( event ) {
+    var x = event.pageX,
+        y = event.pageY;
+
+    h = x / window.innerWidth * 360;
+    s = ( window.innerHeight - y ) / window.innerHeight * 100;
+
+    update();
   });
+
+  window.addEventListener( 'wheel', function( event ) {
+    event.preventDefault();
+    l = clamp( l - event.deltaY, 0, 100 );
+    update();
+  });
+
+  update();
 }) ( window, document );
