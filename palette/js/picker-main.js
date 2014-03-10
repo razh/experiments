d3171f3ded1377e309aaa6acca6f9db6c399fec0
Raymond Zhou <somecrazyidiot@gmail.com>
2014-03-10 11:32:42 -0400

    palette[picker]: Add palette addition/removal.

--- palette/js/picker-main.js
+++ palette/js/picker-main.js
@@ -5,24 +5,28 @@
     return Math.min( Math.max( value, min ), max );
   }
 
-  var hslEl = document.createElement( 'div' );
-  hslEl.classList.add( 'hsl' );
-  document.body.appendChild( hslEl );
+  var paletteEl = document.querySelector( '.palette' );
+  var hslEl = document.querySelector( '.hsl' );
 
   var h = 0,
       s = 50,
       l = 50;
 
-  function update() {
-    var hsl = 'hsl(' +
+  function hslString( h, s, l ) {
+    return 'hsl(' +
       Math.round( h ) + ', ' +
       Math.round( s ) + '%, ' +
       Math.round( l ) + '%)';
+  }
+
+  function update() {
+    var hsl = hslString( h, s, l );
 
     document.body.style.backgroundColor = hsl;
     hslEl.textContent = hsl;
   }
 
+  // Update background color.
   window.addEventListener( 'mousemove', function( event ) {
     var x = event.pageX,
         y = event.pageY;
@@ -40,4 +44,26 @@
   });
 
   update();
+
+  // Add color to palette.
+  function rectMouseDown( event ) {
+    event.stopPropagation();
+
+    var el = event.currentTarget;
+    if ( el.parentNode ) {
+      el.parentNode.removeChild( el );
+      el.removeEventListener( 'mousedown', rectMouseDown );
+    }
+  }
+
+  window.addEventListener( 'mousedown', function( event ) {
+    var rectEl = document.createElement( 'div' );
+
+    rectEl.classList.add( 'rect' );
+    rectEl.style.backgroundColor = hslString( h, s, l );
+    rectEl.addEventListener( 'mousedown', rectMouseDown );
+
+    paletteEl.appendChild( rectEl );
+  });
+
 }) ( window, document );
