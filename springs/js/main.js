c4050559ba9a50b4e2a3d86feecc37f5e228cdba
Raymond Zhou <somecrazyidiot@gmail.com>
2014-04-26 23:29:54 -0400

    springs: Working testbed interaction.

--- springs/js/main.js
+++ springs/js/main.js
@@ -1,4 +1,4 @@
-/*globals Spring*/
+/*globals Spring, requestAnimationFrame*/
 (function( window, document, undefined ) {
   'use strict';
 
@@ -9,15 +9,55 @@
 
   var element = document.querySelector( '.spring' );
 
-  var spring = new Spring();
+  var springs = {
+    x: new Spring( 30, 3 ),
+    y: new Spring( 30, 3 )
+  };
+
+  function setTransform( el, x, y ) {
+    var transform = 'translate3d(' +
+      x + 'px, ' +
+      y + 'px, 0)';
+
+    el.style.webkitTransform = transform;
+    el.style.transform = transform;
+  }
 
   window.addEventListener( 'mousedown', function( event ) {
-    var transform = 'translate3d(' +
-      event.pageX + 'px, ' +
-      event.pageY + 'px, 0)';
-
-    element.style.webkitTransform = transform;
-    element.style.transform = transform;
+    springs.x.setEnd( event.pageX );
+    springs.y.setEnd( event.pageY );
   });
+
+
+  (function() {
+    var prevTime = Date.now(),
+        currTime;
+
+    var halfWidth  = 0.5 * window.innerWidth,
+        halfHeight = 0.5 * window.innerHeight;
+
+    springs.x.start = halfWidth;
+    springs.x.end = halfWidth;
+    springs.x.state.position = halfWidth;
+
+    springs.y.start = halfHeight;
+    springs.y.end = halfHeight;
+    springs.y.state.position = halfHeight;
+
+    function animate() {
+      currTime = Date.now();
+      var dt = currTime - prevTime;
+      prevTime = currTime;
+
+      springs.x.update( dt );
+      springs.y.update( dt );
+
+      setTransform( element, springs.x.state.position, springs.y.state.position );
+
+      requestAnimationFrame( animate );
+    }
+
+    animate();
+  }) ();
 
 }) ( window, document );
