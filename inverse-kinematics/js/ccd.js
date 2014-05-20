e8548bb8fcb75d9b96accb5f2d2e59bf0346b764
Raymond Zhou <somecrazyidiot@gmail.com>
2014-05-19 23:09:21 -0400

    ik[ccd]: Working implementation.
    - Rename effectors array to links.
    - Add CCD calculate method.
    - Remove canvas rotate methods.
    - CCD fromArray now sets initial link positions.
    - Some possible numerical precision errors remain.

--- inverse-kinematics/js/ccd.js
+++ inverse-kinematics/js/ccd.js
@@ -2,47 +2,143 @@
 var CCD = (function() {
   'use strict';
 
-  function Effector( length, angle ) {
+  function Link( length, angle ) {
     this.length = length || 0;
     this.angle = angle || 0;
 
     // Computed values.
-    this.x = this.length;
+    this.x = 0;
     this.y = 0;
-    this.cos = 1;
-    this.sin = 0;
   }
 
   /**
    * Cyclic coordinate descent in two dimensions.
    */
   function CCD( x, y ) {
+    // Origin.
     this.x = x || 0;
     this.y = y || 0;
 
-    this.effectors = [];
+    // End effector position.
+    this.xf = 0;
+    this.yf = 0;
+
+    /**
+     *  Links consist of a joint angle and length.
+     *
+     *             o---C
+     *           /
+     *     o---o
+     *
+     *  origin
+     */
+    this.links = [];
 
     this.EPSILON = 1e-5;
-    this.MAX_ITERATIONS = 20;
+    this.MAX_ITERATIONS = 1;
+
+    this.debug = [];
+    this.debugLines = [];
   }
 
   CCD.prototype.set = function( x, y ) {
+    x -= this.x;
+    y -= this.y;
+    this.debug = [];
+    this.debugLines = [];
+
     var iterations = 0;
     var distance = Number.POSITIVE_INFINITY;
 
-    var count = this.effectors.length;
+    this.calculate();
+
+    var xf = this.xf,
+        yf = this.yf;
+
     var i;
+    var link;
+    var xi, yi;
+    var cos, sin;
+    var da;
+    var dxi, dyi;
+    var dxf, dyf;
+    var rxf, ryf;
+    while ( iterations < this.MAX_ITERATIONS && distance > this.EPSILON ) {
+      for ( i = this.links.length - 1; i >= 0; i-- ) {
+        link = this.links[i];
 
-    var effector;
+        xi = link.x;
+        yi = link.y;
+
+        // Distance from point to current link.
+        dxi = x - xi;
+        dyi = y - yi;
+
+        // Distance from end effector to current link.
+        dxf = xf - xi;
+        dyf = yf - yi;
+
+        var ai = Math.atan2( dyi, dxi );
+        var af = Math.atan2( dyf, dxf );
+        da = ai - af;
+
+        // Rotate end effector about origin of current link.
+        if ( da ) {
+          cos = Math.cos( da );
+          sin = Math.sin( da );
+
+          rxf = cos * dxf - sin * dyf;
+          ryf = sin * dxf + cos * dyf;
+
+          dxf = rxf;
+          dyf = ryf;
+        }
+
+        link.angle += da;
+
+        xf = xi + dxf;
+        yf = yi + dyf;
+        this.debug.push( [ xf + this.x, yf + this.y ] );
+
+        dxf = x - xf;
+        dyf = y - yf;
+
+        distance = Math.sqrt( dxf * dxf + dyf * dyf );
+      }
+
+      iterations++;
+    }
+  };
+
+  CCD.prototype.calculate = function() {
+    var link;
+    var next;
+    var angle;
     var dx, dy;
-    while ( iterations > this.MAX_ITERATIONS || distance < this.EPSILON ) {
-      for ( i = count - 1; i >= 0; i-- ) {
-        effector = this.effectors[i];
+    for ( var i = 0, il = this.links.length; i < il; i++ ) {
+      link = this.links[i];
+      next = this.links[ i + 1 ];
 
-        dx = x - effector.x;
-        dy = y - effector.y;
+      angle = link.angle;
 
-        effector.angle = Math.atan2( dy, dx );
+      dx = link.length;
+      dy = 0;
+
+      if ( angle ) {
+        dx = Math.cos( angle ) * link.length;
+        dy = Math.sin( angle ) * link.length;
+      }
+
+      dx += link.x;
+      dy += link.y;
+
+      if ( next ) {
+        next.x = dx;
+        next.y = dy;
+      } else {
+        // Set end effector position.
+        this.xf = dx;
+        this.yf = dy;
       }
     }
   };
@@ -53,40 +149,52 @@ var CCD = (function() {
     ctx.translate( this.x, this.y );
     ctx.moveTo( 0, 0 );
 
-    var effector;
-    for ( var i = 0, il = this.effectors.length; i < il; i++ ) {
-      effector = this.effectors[i];
+    var link;
+    var cos, sin;
+    for ( var i = 0, il = this.links.length; i < il; i++ ) {
+      link = this.links[i];
 
-      ctx.rotate( effector.angle );
-      ctx.translate( effector.length, 0 );
+      cos = Math.cos( link.angle );
+      sin = Math.sin( link.angle );
+
+      ctx.translate( cos * link.length, sin * link.length );
       ctx.lineTo( 0, 0 );
     }
 
     ctx.restore();
   };
 
-  CCD.prototype.remove = function( effector ) {
-    var index = this.effectors.indexOf( effector );
+  CCD.prototype.remove = function( link ) {
+    var index = this.links.indexOf( link );
 
     if ( index !== -1 ) {
-      this.effectors.splice( index, 1 );
+      this.links.splice( index, 1 );
     }
   };
 
+  /**
+   * Generates an initially horizontal system from an array of link/bone
+   * lengths.
+   */
   CCD.prototype.fromArray = function( array ) {
-    this.effectors = array.reduce(function( effectors, length, index ) {
-      var effector = new Effector( length );
+    this.links = array.reduce(function( links, length, index ) {
+      var link = new Link( length );
 
-      // Set initial position.
+      // Set initial position of link.
       var prev;
-      if ( effectors.length ) {
-        prev = effectors[ index - 1 ];
-        effector.x = prev.x + prev.length;
+      if ( links.length > 1 ) {
+        prev = links[ index - 1 ];
+        link.x = prev.x + prev.length;
       }
 
-      effectors.push( new Effector( length ) );
-      return effectors;
+      links.push( new Link( length ) );
+      return links;
     }, [] );
+
+    // Set end effector position.
+    var last = this.links[ this.links.length - 1 ];
+    this.xf = last.x + last.length;
+    this.yf = 0;
   };
 
   return CCD;
