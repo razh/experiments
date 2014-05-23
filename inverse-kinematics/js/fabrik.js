f082b9048d164748e723277ab96622579fbdf90a
Raymond Zhou <somecrazyidiot@gmail.com>
2014-05-22 23:52:47 -0400

    ik[fabrik]: Handle unreachable target case.

--- inverse-kinematics/js/fabrik.js
+++ inverse-kinematics/js/fabrik.js
@@ -1,6 +1,11 @@
+/*globals IK*/
 /*exported Fabrik*/
 var Fabrik = (function() {
   'use strict';
+
+  function lerp( a, b, t ) {
+    return a + t * ( b - a );
+  }
 
   /**
    * Forward And Backward Reaching Inverse Kinematics.
@@ -20,6 +25,37 @@ var Fabrik = (function() {
   Fabrik.prototype.set = function( x, y ) {
     x -= this.x;
     y -= this.y;
+
+    // Distances.
+    var distance = Math.sqrt( x * x + y * y );
+    var dx, dy;
+    var di;
+
+    // Parameter.
+    var t;
+
+    var count = this.lengths.length;
+    var link;
+    var next;
+    var i;
+    if ( distance > IK.length( this ) ) {
+      // Not reachable.
+      for ( i = 0; i < count; i++ ) {
+        link = IK.links[i];
+
+        // Distance from link to point.
+        dx = x - link.x;
+        dy = y - link.y;
+
+        di = Math.sqrt( dx * dx + dy * dy );
+        t = link.length / di;
+
+        next.x = lerp( link.x, x, t );
+        next.y = lerp( link.y, y, t );
+      }
+    } else {
+      // Reachable.
+    }
   };
 
   return Fabrik;
