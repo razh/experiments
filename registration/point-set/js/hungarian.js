10b1951a88d4440183af595057902c760397aba9
Raymond Zhou <somecrazyidiot@gmail.com>
2014-06-02 22:52:52 -0400

    registration[points]: Use col(s) abbreviation for column(s).

--- registration/point-set/js/hungarian.js
+++ registration/point-set/js/hungarian.js
@@ -25,35 +25,38 @@ var Hungarian = (function() {
   /**
    * Takes a two-dimensional n by m matrix.
    */
-  function subtractColumnMinima( array, columnIndex ) {
+  function subtractColMinima( array, colIndex ) {
     var min = Number.POSITIVE_INFINITY;
     var value;
     var i, il;
     // Create col array and find min.
     for ( i = 0, il = array.length; i < il; i++ ) {
-      value = array[i][ columnIndex ];
+      value = array[i][ colIndex ];
       if ( value < min ) {
         min = value;
       }
     }
 
     for ( i = 0, il = array.length; i < il; i++ ) {
-      array[i][ columnIndex ] -= min;
+      array[i][ colIndex ] -= min;
     }
 
     return array;
   }
 
-  function subtractColumnsMinima( array ) {
+  function subtractColsMinima( array ) {
     var i, il;
     for ( i = 0, il = array[0].length; i < il; i++ ) {
-      subtractColumnMinima( array, i );
+      subtractColMinima( array, i );
     }
   }
 
   function calculate( costMatrix ) {
+    var coveredRows = [],
+        coveredCols = [];
+
     subtractRowsMinima( costMatrix );
-    subtractColumnsMinima( costMatrix );
+    subtractColsMinima( costMatrix );
   }
 
   return {
