ba350db6bd10d21409538b1d9024f3083f7cd2dc
Raymond Zhou <somecrazyidiot@gmail.com>
2014-06-09 02:01:34 -0400

    registration[points]: Add test data.

--- registration/point-set/js/hungarian-test.js
+++ registration/point-set/js/hungarian-test.js
@@ -55,6 +55,53 @@
       ],
       solution: [ 2, 1, 0 ],
       cost: 10
+    },
+    // From: github.com/tdedecko/hungarian-algorithm
+    {
+      matrix: [
+        [ 4, 2, 8 ],
+        [ 4, 3, 7 ],
+        [ 3, 1, 6 ]
+      ],
+      solution: [ 1, 2, 0 ],
+      cost: 12
+    },
+    // From: github.com/bmc/munkres
+    {
+      matrix: [
+        [ 400, 150, 400 ],
+        [ 400, 450, 600 ],
+        [ 300, 225, 300 ]
+      ],
+      solution: [ 1, 0, 2 ],
+      cost: 850
+    },
+    {
+      matrix: [
+        [ 400, 150, 400, 1 ],
+        [ 400, 450, 600, 2 ],
+        [ 300, 225, 300, 3 ]
+      ],
+      solution: [ 1, 3, 0 ],
+      cost: 452
+    },
+    {
+      matrix: [
+        [ 10, 10, 8 ],
+        [  9,  8, 1 ],
+        [  9,  7, 4 ],
+      ],
+      solution: [ 0, 2, 1 ],
+      cost: 18
+    },
+    {
+      matrix: [
+        [ 10, 10, 8, 11 ],
+        [  9,  8, 1,  1 ],
+        [  9,  7, 4, 10 ]
+      ],
+      solution: [ 0, 3, 2 ],
+      cost: 15
     }
   ]);
 
