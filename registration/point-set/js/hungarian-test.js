121fcff11f2a55770d5b0e04173068d3c1a3e620
Raymond Zhou <somecrazyidiot@gmail.com>
2014-06-09 01:39:13 -0400

    registration[points]: Add basic test suite.

--- registration/point-set/js/hungarian-test.js
+++ registration/point-set/js/hungarian-test.js
@@ -2,15 +2,60 @@
 (function( window, document, undefined ) {
   'use strict';
 
-  var matrix = [
-    [ 1, 2, 3 ],
-    [ 2, 4, 6 ],
-    [ 3, 6, 9 ]
-  ];
+  /**
+   * Determine equality of one dimensional numeric arrays.
+   */
+  function arrayEquals( a, b ) {
+    if ( a === b ) {
+      return true;
+    }
 
-  console.log( Hungarian.Matrix.log( matrix ) );
-  var solution = Hungarian.calculate( Hungarian.Matrix.clone( matrix ) );
-  console.log( solution );
-  console.log( Hungarian.cost( matrix, solution ) );
+    if ( a.length !== b.length ) {
+      return false;
+    }
+
+    for ( var i = 0, il = a.length; i < il; i++ ) {
+      if ( a[i] !== b[i] ) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function suite( specs ) {
+    var passed = 0;
+
+    specs.forEach(function( spec, index ) {
+      var clone = Hungarian.Matrix.clone( spec.matrix );
+      var solution = Hungarian.calculate( clone );
+      var cost = Hungarian.cost( spec.matrix, solution );
+
+      if ( arrayEquals( solution, spec.solution ) && cost === spec.cost ) {
+        passed++;
+        console.log( '.' );
+      } else {
+        console.log(
+          index +
+          ': actual: (' + JSON.stringify( solution ) +
+          ', cost: ' + cost + ')' +
+          ', expected: (' + JSON.stringify( spec.solution ) +
+          ', cost: ' +  spec.cost + ')'
+        );
+      }
+    });
+  }
+
+  suite([
+    {
+      matrix: [
+        [ 1, 2, 3 ],
+        [ 2, 4, 6 ],
+        [ 3, 6, 9 ]
+      ],
+      solution: [ 2, 1, 0 ],
+      cost: 10
+    }
+  ]);
 
 }) ( window, document );
