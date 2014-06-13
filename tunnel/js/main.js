b33d3ff2c0230c257bb7b5341fc589e6d935b197
Raymond Zhou <somecrazyidiot@gmail.com>
2014-06-12 23:34:58 -0400

    tunnel: Add regular polygon function.

--- tunnel/js/main.js
+++ tunnel/js/main.js
@@ -1,3 +1,21 @@
 (function( window, document, undefined ) {
   'use strict';
+
+  var PI2 = 2 * Math.PI;
+
+  var Shape = {
+    regularPolygon: function( sides ) {
+      var angle = -PI2 / sides;
+
+      var vertices = [];
+
+      for ( var i = 0; i < sides; i++ ) {
+        vertices.push( Math.cos( i * angle ) );
+        vertices.push( Math.sin( i * angle ) );
+      }
+
+      return vertices;
+    }
+  };
+
 }) ( window, document );
