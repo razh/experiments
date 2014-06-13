9cf9b12e51c5c7b7ce5847de2fbf85d099c1ef2f
Raymond Zhou <somecrazyidiot@gmail.com>
2014-06-13 02:08:51 -0400

    tunnel: Add iterated polygon drawing.

--- tunnel/js/main.js
+++ tunnel/js/main.js
@@ -3,8 +3,8 @@
 
   var PI2 = 2 * Math.PI;
 
-  var Shape = {
-    regularPolygon: function( sides ) {
+  var Geometry = {
+    createRegularPolygon: function( sides ) {
       var angle = -PI2 / sides;
 
       var vertices = [];
@@ -15,7 +15,58 @@
       }
 
       return vertices;
+    },
+
+    drawPolygon: function( ctx, vertices ) {
+      if ( !vertices.length ) {
+        return;
+      }
+
+      ctx.beginPath();
+
+      ctx.moveTo( vertices[0], vertices[1] );
+      for ( var i = 1, il = 0.5 * vertices.length; i < il; i++ ) {
+        ctx.lineTo( vertices[ 2 * i ], vertices[ 2 * i + 1 ] );
+      }
+
+      ctx.closePath();
     }
   };
+
+  var canvas  = document.querySelector( 'canvas' ),
+      context = canvas.getContext( '2d' );
+
+  function init() {
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+
+  var pentagon = Geometry.createRegularPolygon(5);
+
+  function draw( ctx ) {
+    var width  = ctx.canvas.width,
+        height = ctx.canvas.height;
+
+    var radius = Math.max( width, height );
+
+    ctx.save();
+    ctx.translate( 0.5 * width, 0.5 * height );
+
+    for ( var i = 10; i < radius; i += 10 ) {
+      ctx.save();
+      ctx.scale( i, i );
+      Geometry.drawPolygon( ctx, pentagon );
+      ctx.restore();
+
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = '#fff';
+      ctx.stroke();
+    }
+
+    ctx.restore();
+  }
+
+  init();
+  draw( context );
 
 }) ( window, document );
