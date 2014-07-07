8f92c54410a4fbe0b094e6e1b16b5c9377ff4422
Raymond Zhou <somecrazyidiot@gmail.com>
2014-07-06 23:55:04 -0400

    normal-map[css]: Add normal-map CPU shader function.
    Implementation based off GLSL example by mattdesl:
    https://github.com/mattdesl/lwjgl-basics/wiki/ShaderLesson6

--- normal-map/js/css-box-shadow.js
+++ normal-map/js/css-box-shadow.js
@@ -113,6 +113,139 @@
     return shadows.join( ', ' );
   }
 
+  /**
+   * Canvas implementation of mattdesl's normal map shader.
+   * https://github.com/mattdesl/lwjgl-basics/wiki/ShaderLesson6
+   */
+  var shaderFn = (function() {
+    // Temporary canvas.
+    var canvas = document.createElement( 'canvas' );
+    var ctx = canvas.getContext( '2d' );
+
+    return function shaderFn( light, ambient, falloff, diffuse, normal ) {
+      // Light position.
+      var lx = light.x,
+          ly = light.y,
+          lz = light.z;
+
+      // Light color.
+      var lr = light.r,
+          lg = light.g,
+          lb = light.b,
+          la = light.a;
+
+      // Pre-multiply light color with intensity.
+      lr *= la;
+      lg *= la;
+      lb *= la;
+
+      // Ambient color.
+      var ar = ambient.r,
+          ag = ambient.g,
+          ab = ambient.b,
+          aa = ambient.a;
+
+      // Pre-multiply ambient color with intensity.
+      ar *= aa;
+      ag *= aa;
+      ab *= aa;
+
+      // Falloff (attenuation coefficients).
+      var fx = falloff.x,
+          fy = falloff.y,
+          fz = falloff.z;
+
+      var diffuseData = diffuse.data,
+          normalData  = normal.data;
+
+      var imageData = ctx.createImageData( diffuse );
+
+      var data   = imageData.data,
+          width  = imageData.width,
+          height = imageData.height;
+
+      var x, y;
+      var index;
+      var dlx, dly, dlz;
+      var D, Dinverse;
+      // Normalized normal and light vectors.
+      var Nr, Ng, Nb;
+      var Lx, Ly, Lz;
+      // N dot L.
+      var NL;
+      // Diffuse color.
+      var dr, dg, db, da;
+      // Normal color.
+      var nr, ng, nb;
+      // Length of normal vector (inverse).
+      var ln;
+      // Diffuse: light color * Lambertian reflection.
+      var Dr, Dg, Db;
+      // Attenuation and intensity color.
+      var attenuation;
+      var ir, ig, ib;
+      for ( y = 0; y < height; y++ ) {
+        for ( x = 0; x < width; x++ ) {
+          index = 4 * ( y * width + x );
+
+          dr = diffuseData[ index ];
+          dg = diffuseData[ index + 1 ];
+          db = diffuseData[ index + 2 ];
+          da = diffuseData[ index + 3 ];
+
+          nr = normalData[ index ];
+          ng = normalData[ index + 1 ];
+          nb = normalData[ index + 2 ];
+
+          // Light delta position.
+          dlx = lx - x;
+          dly = ly - y;
+          dlz = lz;
+
+          // Light distance.
+          D = Math.sqrt( dlx * dlx + dly * dly + dlz * dlz );
+
+          // Normalize.
+          nr = nr * 2 - 1;
+          ng = ng * 2 - 1;
+          nb = nb * 2 - 1;
+          ln = 1 / Math.sqrt( nr * nr + ng * ng + nb * nb );
+
+          Nr = nr * ln;
+          Ng = ng * ln;
+          Nb = nb * ln;
+
+          Dinverse = 1 / D;
+          Lx = dlx * Dinverse;
+          Ly = dly * Dinverse;
+          Lz = dlz * Dinverse;
+
+          // N dot L.
+          NL = Math.max( Nr * Lx + Ng * Ly + Nb * Lz, 0 );
+          // Diffuse lighting factor.
+          Dr = lr * NL;
+          Dg = lg * NL;
+          Db = lb * NL;
+
+          attenuation = 1 / ( fx + ( fy * D ) + ( fz * D * D ) );
+
+          // Intensity = Ambient + Diffuse * Attenuation.
+          ir = ar + Dr * attenuation;
+          ig = ag + Dg * attenuation;
+          ib = ab + Db * attenuation;
+
+          // Final color (diffuse color * intensity).
+          data[ index     ] = dr * ir;
+          data[ index + 1 ] = dg * ig;
+          data[ index + 2 ] = db * ib;
+          data[ index + 3 ] = da;
+        }
+      }
+
+      return data;
+    };
+  }) ();
+
   // Input.
   var inputs = {
     scale: document.querySelector( '#scale' ),
