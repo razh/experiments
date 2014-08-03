e6df3dc4a4576573c75e279a378cf593af3a4519
Raymond Zhou <somecrazyidiot@gmail.com>
2014-08-02 23:12:35 -0400

    palette[generator]: Add RGBtoHSV function.
    From Skia’s SkColor class.

--- palette/generator/js/color.js
+++ palette/generator/js/color.js
@@ -22,6 +22,46 @@ var Color = (function() {
     return ( red << 16 ) | ( green << 8 ) | blue;
   }
 
+  // From SkRGBToHSV in Skia's SkColor.cpp.
+  // https://github.com/google/skia/blob/master/src/core/SkColor.cpp
+  function RGBtoHSV( red, green, blue, hsv ) {
+    var min = Math.min( red, green, blue );
+    var max = Math.max( red, green, blue );
+    var delta = max - min;
+
+    var v = max / 255;
+
+    // Shade of gray.
+    if ( !delta ) {
+      hsv[0] = 0;
+      hsv[1] = 0;
+      hsv[2] = v;
+      return hsv;
+    }
+
+    var s = delta / max;
+    var h;
+    if ( red === max ) {
+      h = ( green - blue ) / delta;
+    } else if ( green === max ) {
+      h = 2 * ( blue - red ) / delta;
+    } else {
+      // blue === max.
+      h = 4 * ( red - green ) / delta;
+    }
+
+    h *= 60;
+    if ( h < 0 ) {
+      h += 360;
+    }
+
+    hsv[0] = h;
+    hsv[1] = s;
+    hsv[2] = v;
+
+    return hsv;
+  }
+
   return {
     BLACK: BLACK,
     WHITE: WHITE,
@@ -29,7 +69,9 @@ var Color = (function() {
     red: red,
     green: green,
     blue: blue,
-    rgb: rgb
+    rgb: rgb,
+
+    RGBtoHSV: RGBtoHSV
   };
 
 }) ();
