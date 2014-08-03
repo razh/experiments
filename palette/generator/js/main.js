75894616784f6136d34f7e401589126412648e04
Raymond Zhou <somecrazyidiot@gmail.com>
2014-08-03 12:35:33 -0400

    palette[generator]: Display output colors.

--- palette/generator/js/main.js
+++ palette/generator/js/main.js
@@ -1,10 +1,19 @@
-/*global DominantColorCalculator*/
+/*global Color, DominantColorCalculator*/
 (function( window, document, undefined ) {
   'use strict';
+
+  var colorsEl = document.querySelector( '.colors' );
 
   var canvas  = document.createElement( 'canvas' ),
       context = canvas.getContext( '2d' );
 
+  function intToRgb( color ) {
+    return 'rgb(' +
+      Color.red(   color ) + ', ' +
+      Color.green( color ) + ', ' +
+      Color.blue(  color ) +
+    ')';
+  }
 
   function prepend( el ) {
     document.body.insertBefore( el, document.body.firstChild );
@@ -14,14 +23,28 @@
   image.onload = function() {
     canvas.width = image.naturalWidth;
     canvas.height = image.naturalHeight;
-    context.drawImage( image, 0, 0 );
+    context.drawImage( image, 0, 0, canvas.width, canvas.height );
+    prepend( canvas );
 
+    console.time( 'color' );
     var calculator = new DominantColorCalculator(
       context.getImageData( 0, 0, canvas.width, canvas.height )
     );
+    console.timeEnd( 'color' );
 
-    console.log( calculator.colorScheme );
-    prepend( image );
+    var colorScheme = calculator.colorScheme;
+
+    console.log( colorScheme );
+    Object.keys( colorScheme ).forEach(function( key ) {
+      var el = document.createElement( 'div' );
+      el.className = 'color';
+
+      var color = intToRgb( colorScheme[ key ] );
+      el.style.backgroundColor = color;
+      el.setAttribute( 'data-color', color );
+
+      colorsEl.appendChild( el );
+    });
   };
 
   document.addEventListener( 'drop', function( event ) {
